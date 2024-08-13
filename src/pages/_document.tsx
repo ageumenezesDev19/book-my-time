c9d0d5b15f3a4381f724012eeb6301b42e1eebf5
ageu_dev19 <ageumenezes423@gmail.com>
2024-08-13 02:03:42 -0300

    Design System Installation

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
+import { getCssText } from "@ignite-ui/react";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"></link>
+      
+      <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
       <Head />
-      <body style={{
-        background: '#000000',
-        color: '#fff'
-      }}>
+      <body>
         <Main />
         <NextScript />
       </body>

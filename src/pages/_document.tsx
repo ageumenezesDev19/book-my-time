229c87b55f24f36ee907748c6c5f5070875c8fdd
ageu_dev19 <ageumenezes423@gmail.com>
2024-08-14 01:38:46 -0300

    ESLint Configuration

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,20 @@
-import { getCssText } from "@ignite-ui/react";
-import { Html, Head, Main, NextScript } from "next/document";
+import { getCssText } from '@ignite-ui/react'
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"></link>
-      
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+        rel="stylesheet"
+      ></link>
+
       <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
       <Head />
       <body>
@@ -15,5 +22,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }

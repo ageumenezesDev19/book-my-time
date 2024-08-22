b0b3a3ff3b8fca3128fbd570db2776cd069db747
ageu_dev19 <ageumenezes423@gmail.com>
2024-08-22 10:15:42 -0300

    Setting up availability form

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -16,11 +16,43 @@ import {
   IntervalsContainer,
 } from './styles'
 import { ArrowRight } from 'phosphor-react'
+// import { z } from 'zod'
+import { useFieldArray, useForm } from 'react-hook-form'
+import { getWeekDays } from '@/utils/get-week-days'
+
+// const timeIntervalsFormSchema = z.object({})
 
 export default function TimeIntervals() {
   // async function handleNavigateToNextStep() {
   //   await router.push('/register/time-intervals')
   // }
+  const {
+    register,
+    handleSubmit,
+    control,
+    // formState: { isSubmitting, erros },
+  } = useForm({
+    defaultValues: {
+      intervals: [
+        { weekDay: 0, enabled: false, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 1, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 2, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 3, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 4, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 5, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 6, enabled: false, startTime: '08:00', endTime: '18:00' },
+      ],
+    },
+  })
+
+  const { fields } = useFieldArray({
+    control,
+    name: 'intervals',
+  })
+
+  const weekDays = getWeekDays()
+
+  async function handleSetTimeIntervals() {}
 
   return (
     <>
@@ -35,56 +67,38 @@ export default function TimeIntervals() {
           <MultiStep size={4} currentStep={3} />
         </Header>
 
-        <IntervalBox as="form">
+        <IntervalBox as="form" onSubmit={handleSubmit(handleSetTimeIntervals)}>
           <IntervalsContainer>
-            <IntervalItem>
-              <IntervalDay>
-                <Checkbox />
-                <Text>Segunda-feira</Text>
-              </IntervalDay>
-              <IntervalInputs>
-                <TextInput
-                  size="sm"
-                  type="time"
-                  step={60}
-                  onPointerEnterCapture={undefined}
-                  onPointerLeaveCapture={undefined}
-                  crossOrigin={undefined}
-                />
-                <TextInput
-                  size="sm"
-                  type="time"
-                  step={60}
-                  onPointerEnterCapture={undefined}
-                  onPointerLeaveCapture={undefined}
-                  crossOrigin={undefined}
-                />
-              </IntervalInputs>
-            </IntervalItem>
-            <IntervalItem>
-              <IntervalDay>
-                <Checkbox />
-                <Text>Terça-feira</Text>
-              </IntervalDay>
-              <IntervalInputs>
-                <TextInput
-                  size="sm"
-                  type="time"
-                  step={60}
-                  onPointerEnterCapture={undefined}
-                  onPointerLeaveCapture={undefined}
-                  crossOrigin={undefined}
-                />
-                <TextInput
-                  size="sm"
-                  type="time"
-                  step={60}
-                  onPointerEnterCapture={undefined}
-                  onPointerLeaveCapture={undefined}
-                  crossOrigin={undefined}
-                />
-              </IntervalInputs>
-            </IntervalItem>
+            {fields.map((field, index) => {
+              return (
+                <IntervalItem key={field.id}>
+                  <IntervalDay>
+                    <Checkbox />
+                    <Text>{weekDays[field.weekDay]}</Text>
+                  </IntervalDay>
+                  <IntervalInputs>
+                    <TextInput
+                      size="sm"
+                      type="time"
+                      step={60}
+                      {...register(`intervals.${index}.startTime`)}
+                      onPointerEnterCapture={undefined}
+                      onPointerLeaveCapture={undefined}
+                      crossOrigin={undefined}
+                    />
+                    <TextInput
+                      size="sm"
+                      type="time"
+                      step={60}
+                      {...register(`intervals.${index}.endTime`)}
+                      onPointerEnterCapture={undefined}
+                      onPointerLeaveCapture={undefined}
+                      crossOrigin={undefined}
+                    />
+                  </IntervalInputs>
+                </IntervalItem>
+              )
+            })}
           </IntervalsContainer>
 
           <Button type="submit">

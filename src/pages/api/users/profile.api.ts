4dd59ce27373c9f9847b78d21bc538568ddcd934
ageu_dev19 <ageumenezes423@gmail.com>
2024-08-26 15:26:51 -0300

    Persisting profile

---
+++ src/pages/api/users/profile.api.ts
@@ -0,0 +1,46 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getServerSession } from 'next-auth'
+import { z } from 'zod'
+import { prisma } from '../../../lib/prisma'
+import { buildNextAuthOptions } from '../../api/auth/[...nextauth].api'
+
+const updateProfileBodySchema = z.object({
+  bio: z.string(),
+})
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'PUT') {
+    return res.status(405).end()
+  }
+
+  const session = await getServerSession(
+    req,
+    res,
+    buildNextAuthOptions(req, res),
+  )
+
+  if (!session) {
+    return res.status(401).end()
+  }
+
+  const { bio } = updateProfileBodySchema.parse(req.body)
+
+  try {
+    await prisma.user.update({
+      where: {
+        id: session.user.id,
+      },
+      data: {
+        bio,
+      },
+    })
+
+    return res.status(204).end()
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ error: 'Internal Server Error' })
+  }
+}
